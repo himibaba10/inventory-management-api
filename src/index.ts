20a17e8cfc580188f889e4c908757dba8abfb6d4
Ferdous Ahmed <himibaba10@gmail.com>
2025-05-02 16:50:25 +0600

    Integrated supplier route to index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import customerRoute from "./routes/customer.route";
 import userRoute from "./routes/user.route";
 import attendantRoute from "./routes/attendant.route";
 import shopRoute from "./routes/shop.route";
+import supplierRoute from "./routes/supplier.route";
 require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,6 +18,7 @@ app.use("/api/v1/customers", customerRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/attendants", attendantRoute);
 app.use("/api/v1/shops", shopRoute);
+app.use("/api/v1/suppliers", supplierRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
